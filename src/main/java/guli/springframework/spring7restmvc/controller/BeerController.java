10022799bb034bf167dfa8716b4b1cff06456208
Gustavo Nagai Amorim <gulifiree@gmail.com>
2026-03-24 20:31:51 -0300

    Consuming an API using @RequestMapping and @RestController - JSON response - using POSTMAN

--- src/main/java/guli/springframework/spring7restmvc/controller/BeerController.java
+++ src/main/java/guli/springframework/spring7restmvc/controller/BeerController.java
@@ -2,19 +2,30 @@ package guli.springframework.spring7restmvc.controller;
 
 import guli.springframework.spring7restmvc.model.Beer;
 import guli.springframework.spring7restmvc.service.BeerService;
+import jakarta.websocket.server.PathParam;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
 
-    Beer getBeerById(UUID id) {
+    @RequestMapping("api/v1/beer")
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
+
+//    @RequestMapping("api/v1/beer/{id}")
+//    @PathParam('')
+    public Beer getBeerById(UUID id) {
 
         log.info("getBeerById foi chamado - Controller");
 
